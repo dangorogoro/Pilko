bf48e775c267184afe59fe1ee13278d8f28e4e87
dangorogoro <dangorogoro0802@gmail.com>
2018-07-29 14:42:55 +0900

    Add a task for controlling servo motors

--- main/servo_control.c
+++ main/servo_control.c
@@ -1,49 +1,48 @@
-/* servo motor control example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-
 #include "mine.h"
-//You can get these value from the datasheet of servo you use, in general pulse width varies between 1000 to 2000 mocrosecond
+
 #define SERVO_MIN_PULSEWIDTH 600 //Minimum pulse width in microsecond
 #define SERVO_MAX_PULSEWIDTH 2400 //Maximum pulse width in microsecond
 #define SERVO_MAX_DEGREE 180 //Maximum angle in degree upto which servo can rotate
 
-void mcpwm_example_gpio_initialize()
-{
-    printf("initializing mcpwm servo control gpio......\n");
-    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, 18);    //Set GPIO 18 as PWM0A, to which servo is connected
+#define MCPWM_GPIO_A 18
+#define MCPWM_GPIO_B 26
+#define MCPWM_GPIO_C 27
+void motor_control(int32_t motor_select, uint32_t angle){
+  uint32_t duty = servo_per_degree_init(angle);
+  switch(motor_select){
+    case MCPWM_GPIO_A:
+      mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, duty);
+      break;
+    case MCPWM_GPIO_B:
+      mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_B, duty);
+      break;
+    case MCPWM_GPIO_C:
+      mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_1, MCPWM_OPR_A, duty);
+      break;
+  }
+}
+void all_motor_control(uint32_t A_angle, uint32_t B_angle, uint32_t C_angle){
+  motor_control(MCPWM_GPIO_A,A_angle);
+  motor_control(MCPWM_GPIO_B,B_angle);
+  motor_control(MCPWM_GPIO_C,C_angle);
+}
+void mc_pwm_config(){
+  printf("initializing mcpwm servo control gpio......\n");
+  mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, MCPWM_GPIO_A);
+  mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0B, MCPWM_GPIO_B);
+  mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM1A, MCPWM_GPIO_C);
 }
 
-/**
- * @brief Use this function to calcute pulse width for per degree rotation
- *
- * @param  degree_of_rotation the angle in degree to which servo has to rotate
- *
- * @return
- *     - calculated pulse width
- */
-uint32_t servo_per_degree_init(uint32_t degree_of_rotation)
-{
+uint32_t servo_per_degree_init(uint32_t degree_of_rotation){
     uint32_t cal_pulsewidth = 0;
     cal_pulsewidth = (SERVO_MIN_PULSEWIDTH + (((SERVO_MAX_PULSEWIDTH - SERVO_MIN_PULSEWIDTH) * (degree_of_rotation)) / (SERVO_MAX_DEGREE)));
     return cal_pulsewidth;
 }
 
-/**
- * @brief Configure MCPWM module
- */
-void mcpwm_example_servo_control(void *arg)
-{
+void mcpwm_example_servo_control(void *arg){
     uint32_t angle, count;
-    //1. mcpwm gpio initialization
-    mcpwm_example_gpio_initialize();
+    mc_pwm_config();
 
-    //2. initial mcpwm configuration
     printf("Configuring Initial Parameters of mcpwm......\n");
     mcpwm_config_t pwm_config;
     pwm_config.frequency = 50;    //frequency = 50Hz, i.e. for every servo motor time period should be 20ms
@@ -52,21 +51,16 @@ void mcpwm_example_servo_control(void *arg)
     pwm_config.counter_mode = MCPWM_UP_COUNTER;
     pwm_config.duty_mode = MCPWM_DUTY_MODE_0;
     mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config);    //Configure PWM0A & PWM0B with above settings
+    mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_1, &pwm_config);    //Configure PWM0A & PWM0B with above settings
     while (1) {
         for (count = 0; count < SERVO_MAX_DEGREE; count++) {
             printf("Angle of rotation: %d\n", count);
             angle = servo_per_degree_init(count);
             printf("pulse width: %dus\n", angle);
             mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, angle);
+            mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_B, 1500);
             vTaskDelay(10);     //Add delay, since it takes time for servo to rotate, generally 100ms/60degree rotation at 5V
         }
     }
 }
 
-/*
-void app_main()
-{
-    printf("Testing servo motor.......\n");
-    xTaskCreate(mcpwm_example_servo_control, "mcpwm_example_servo_control", 4096, NULL, 5, NULL);
-}
-*/
--- main/servo_control.h
+++ main/servo_control.h
@@ -4,6 +4,11 @@
 #include "driver/mcpwm.h"
 #include "soc/mcpwm_reg.h"
 #include "soc/mcpwm_struct.h"
+typedef struct{
+  uint32_t duty;
+  uint32_t arm_len;
+  uint32_t shaft_len;
+}MotorUnit;
 void mcpwm_example_gpio_initialize();
 uint32_t servo_per_degree_init(uint32_t degree_of_rotation);
 void mcpwm_example_servo_control(void *arg);
